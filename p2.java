2a00ca19531fe741310fdff8b8e6e1cce488e089
Pavan-Nandi <163009167+Pavan-Nandi@users.noreply.github.com>
2024-07-15 09:01:59 +0530

    Create p2.java

---
+++ p2.java
@@ -0,0 +1,13 @@
+ import java.util.*;
+public class p2{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+if(num<15 || num>60){
+System.out.println("Not Allowed");
+}
+else{
+System.out.println("Allowed");
+}
+}
+}
