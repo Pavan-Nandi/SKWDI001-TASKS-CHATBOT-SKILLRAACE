390af3e2f75d7f4a492929d00ba92ff7cabd140b
Pavan-Nandi <163009167+Pavan-Nandi@users.noreply.github.com>
2024-08-17 13:58:55 +0530

    Poll App

--- User.java
+++ User.java
@@ -1,31 +1,37 @@
 public class User {
     private String username;
     private String password;
-    private String email;
-    public User(String username,String password,String email){
-        this.username=username;
-        this.password=password;
-        this.email=email;
+    private boolean hasVoted;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.hasVoted = false;
     }
+
     public String getUsername() {
         return username;
     }
-    public void setUsername(String username) {
-        this.username = username;
+
+    public boolean validatePassword(String password) {
+        return this.password.equals(password);
     }
-    public String getPassword() {
-        return password;
+
+    public boolean hasVoted() {
+        return hasVoted;
     }
+
+    public void setVoted(boolean hasVoted) {
+        this.hasVoted = hasVoted;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
 
-    // Constructors, getters, and setters
+    public void updateProfile(String newUsername, String newPassword) {
+        this.username = newUsername;
+        this.password = newPassword;
+        System.out.println("Profile updated successfully.");
+    }
 }
